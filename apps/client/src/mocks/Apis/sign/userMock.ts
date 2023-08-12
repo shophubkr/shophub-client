4509dd392a79043740ae97a8ffeee89df98b4612
beenbin <showme0241@gmail.com>
2023-08-12 18:21:45 +0900

    :sparkles: chore : msw

---
+++ apps/client/src/mocks/Apis/sign/userMock.ts
@@ -0,0 +1,19 @@
+// 데이터 베이스
+export const userMock = [
+  {
+    userType: "buyer",
+    email: "showme0241@gmail.com",
+    password: "123456789",
+    nickName: "been",
+    tel: "010-1111-1111",
+    businessNum: "000-11-22222",
+  },
+  {
+    userType: "seller",
+    email: "anyone@gmail.com",
+    password: "987654321",
+    nickName: "any",
+    tel: "010-1111-1111",
+    businessNum: "000-55-12522",
+  },
+];
