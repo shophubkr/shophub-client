6e7f1199cb4591f226793fdf1999313bce55bf3f
Doeunnkimm <doeunnkimm@gmail.com>
2023-09-12 16:42:13 +0900

    chore: 테스트를 위한 image domain 추가

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  images: {
+    domains: ["picsum.photos"], // picsum.photos: 테스트 이미지를 위한 domain입니다.
+  },
 };
 
 module.exports = nextConfig;
