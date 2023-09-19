353c08a3647568648abdc1ed03dcc86d0e0d3d71
Doeunnkimm <doeunnkimm@gmail.com>
2023-09-19 22:56:58 +0900

    chore: 쿠폰 만료 시 문구 변경

--- apps/client/src/app/_components/Coupon/components/RestDays.tsx
+++ apps/client/src/app/_components/Coupon/components/RestDays.tsx
@@ -4,7 +4,7 @@ import { Flex, Text } from "@chakra-ui/react";
 import type { RestDayProps } from "./CouponComponents.types";
 
 export const RestDay = ({ restDay, align }: RestDayProps) => {
-  const restDayText = restDay > 0 ? `쿠폰 만료 ${restDay}일 남았어요` : "쿠폰이 종료되었어요";
+  const restDayText = restDay > 0 ? `쿠폰 만료 ${restDay}일 남았어요` : "쿠폰이 만료되었어요";
   return (
     <Flex
       bgColor={restDay > 0 ? "#000" : "#C9C9C9"}
