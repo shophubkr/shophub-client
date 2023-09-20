1baa0a1b64a260537d9d94c2241d113f448288d5
beenbin92 <showme0241@gmail.com>
2023-09-20 18:26:28 +0900

    feat : Kakaomap 컴포넌트 script 생성 방식 수정
    as-is : Next 내장 (정적)
    to-be : create 함수 (동적)
    => 이유 : msw 실행 시점이 빨라지면서 클라이언트 컴포넌트로 모듈이 바꼈고 Script라는 Next 내장 컴포넌트 작동 X

--- apps/client/src/app/(home)/_components/Map/KakaoMap.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const MapWrapper = styled.div`
-  position: absolute;
-  width: 100%;
-  z-index: -1;
-  top: 0;
-  left: 0;
-`;
--- apps/client/src/app/(home)/_components/Map/KakaoMap.tsx
+++ apps/client/src/app/(home)/_components/Map/KakaoMap.tsx
@@ -1,14 +1,29 @@
-import Script from "next/script";
+import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { Map } from "react-kakao-maps-sdk";
-import * as S from "./KakaoMap.style";
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=fee63af963e627ff4561955f5603f656&autoload=false`;
 
 export const KakaoMap = () => {
+  const [kakaoLoaded, setKakaoLoaded] = useState(false);
+
+  useEffect(() => {
+    const kakaoScript = document.createElement("script");
+    kakaoScript.src = KAKAO_SDK_URL;
+    kakaoScript.async = true;
+    kakaoScript.onload = () => {
+      setKakaoLoaded(true);
+    };
+    document.body.appendChild(kakaoScript);
+
+    return () => {
+      document.body.removeChild(kakaoScript);
+    };
+  }, []);
+
   return (
-    <S.MapWrapper>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <Map center={{ lat: 37.5000776, lng: 127.0385419 }} style={{ width: "100%", height: "100vh" }} />
-    </S.MapWrapper>
+    <Box position="absolute" w="100%" zIndex="-1" top="0" left="0">
+      {kakaoLoaded && <Map center={{ lat: 37.5000776, lng: 127.0385419 }} style={{ width: "100%", height: "100vh" }} />}
+    </Box>
   );
 };
