92b5a2ed838ad7db3d5b87c1845b1deeedaf31a3
beenbin92 <showme0241@gmail.com>
2023-09-23 14:38:02 +0900

    feat : add <position : sticky> of StoreList Toggle

--- apps/client/src/app/(home)/_components/StoreList/StoreList.tsx
+++ apps/client/src/app/(home)/_components/StoreList/StoreList.tsx
@@ -5,6 +5,8 @@ import type { ApiProductType } from "../../_types/apiData.type";
 import * as S from "./StoreList.style";
 
 export const StoreList = ({ data }: ApiProductType) => {
+  // 이후, 다양한 동적 관리를 위해 isToggle을 recoil로 관리하여
+  // Api call을 useEffect로 관리하고, 의존성 배열에 담아야 합니다.
   const [isToggle, setIsToggle] = useState(false);
 
   return (
@@ -14,7 +16,9 @@ export const StoreList = ({ data }: ApiProductType) => {
       bottom="0"
       width="100%"
       height={!isToggle ? "88px" : "80vh"}
+      alignItems="flex-start"
       bgColor="white"
+      boxShadow="0 -4px 8px rgba(0,0,0,0.05)"
       overflow={isToggle ? "scroll" : "hidden"}
       transition="0.75s cubic-bezier(0.165, 0.84, 0.44, 1)"
     >
@@ -22,7 +26,7 @@ export const StoreList = ({ data }: ApiProductType) => {
         flexDir="column"
         alignItems="center"
         justifyContent="space-between"
-        position="absolute"
+        position="sticky"
         zIndex="9999"
         top="0"
         h="40px"
