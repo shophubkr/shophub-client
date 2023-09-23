ee16e0b53270d353b5d57d058a35cf4eb4fe1c1e
beenbin92 <showme0241@gmail.com>
2023-09-23 14:35:35 +0900

    feat : modify product 모의 api key명 & add new data by params

--- apps/client/src/mocks/Apis/product/productApi.ts
+++ apps/client/src/mocks/Apis/product/productApi.ts
@@ -4,13 +4,13 @@ import { productMockData } from "./productMock";
 export type ProductType = {
   storeIdx: number;
   storeName: string;
-  catchPraise: string;
-  thumbnail: string;
+  description: string;
+  thumbnailUrl: string;
   latLng: {
     lat: number;
     lng: number;
   };
-  isCoupon: boolean;
+  isCouponAvailable: boolean;
   isRecommend: boolean;
   distance: number;
   price: number;
@@ -29,13 +29,40 @@ export const getProductApi = rest.get<ProductType[]>("/api/products", (req, res,
 
   const data = productMockData.slice(startIndex, endIndex);
 
+  if (storeType === "recommend") {
+    const recommendFiltering = productMockData.filter((it) => it.isRecommend === true);
+    const recommendData = recommendFiltering.slice(startIndex, endIndex);
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        page: pageNum,
+        result: recommendData,
+        totalResult: recommendFiltering.length,
+        totalPages: Math.ceil(recommendFiltering.length / 8),
+      }),
+    );
+  }
+
+  if (storeType === "nearBy") {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        page: pageNum,
+        result: data,
+        totalResult: productMockData.length,
+        totalPages: Math.ceil(productMockData.length / 8),
+      }),
+    );
+  }
+
   return res(
     ctx.status(200),
     ctx.json({
       page: pageNum,
       result: data,
-      total_result: productMockData.length,
-      total_pages: Math.ceil(productMockData.length / 8),
+      totalResult: productMockData.length,
+      totalPages: Math.ceil(productMockData.length / 8),
     }),
   );
 });
