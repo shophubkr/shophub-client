a5258b60c3d9545bc8b2ace9479af37d27aeb5b4
beenbin92 <showme0241@gmail.com>
2023-09-28 19:36:05 +0900

    feat : add key props of map data
    p.s
    > msw 반복기를 통해 데이터를 공급하고 있어 storeIdx에 랜덤 숫자를 임시용으로 붙었습니다.
    > modify 환경변수 <sdk of KakaoMap Component>

--- apps/client/src/app/(home)/_components/Map/KakaoMap.tsx
+++ apps/client/src/app/(home)/_components/Map/KakaoMap.tsx
@@ -4,9 +4,9 @@ import { CustomOverlayMap, Map } from "react-kakao-maps-sdk";
 import type { ApiProductType } from "../../_types/apiData.type";
 import { StorePinOnMap } from "../StorePinOnMap/StorePinOnMap";
 
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=fee63af963e627ff4561955f5603f656&autoload=false`;
+const KAKAO_SDK_URL = process.env.NEXT_PUBLIC_SDK_KAKAO_MAP as string;
 
-export const KakaoMap = ({ data }: ApiProductType) => {
+export const KakaoMap = ({ storeDatas }: ApiProductType) => {
   const [kakaoLoaded, setKakaoLoaded] = useState(false);
 
   useEffect(() => {
@@ -27,9 +27,10 @@ export const KakaoMap = ({ data }: ApiProductType) => {
     <Box position="absolute" w="100%" zIndex="0" top="0" left="0">
       {kakaoLoaded && (
         <Map center={{ lat: 37.500643, lng: 127.036377 }} style={{ width: "100%", height: "100vh" }}>
-          {data.map((it) => (
-            <CustomOverlayMap position={it.latLng}>
-              <StorePinOnMap data={it} />
+          {storeDatas.map((store) => (
+            // api 통신 이후, storeIdx만 key prop 설정 예정
+            <CustomOverlayMap key={store.storeIdx + Math.random()} position={store.latLng}>
+              <StorePinOnMap store={store} />
             </CustomOverlayMap>
           ))}
         </Map>
--- apps/client/src/app/_components/Card/MainStoreCard.tsx
+++ apps/client/src/app/_components/Card/MainStoreCard.tsx
@@ -1,13 +1,14 @@
 import { Center } from "@chakra-ui/react";
 import type { ProductType } from "~/mocks/Apis/product/productApi";
-import { Card } from "./_components/Card";
+import { Card } from "./_components";
 
-export const MainStoreCard = ({ data }: { data: ProductType[] }) => {
+export const MainStoreCard = ({ storeDatas }: { storeDatas: ProductType[] }) => {
   return (
     <Center position="absolute" top="0" flexDir="column" w="100%" height="auto" padding="40px 0 48px">
-      {data.map((it) => {
-        return <Card data={it} />;
-      })}
+      {storeDatas.map((store) => (
+        // api 통신 이후, storeIdx만 key prop 설정 예정
+        <Card key={store.storeIdx + Math.random()} store={store} />
+      ))}
     </Center>
   );
 };
