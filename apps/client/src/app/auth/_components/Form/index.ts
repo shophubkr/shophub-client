1de012d8aa4738601f9a5c74922e54e76a13e639
been bin <showme0241@gmail.com>
2023-10-21 18:45:51 +0900

    feat : delete components in '/signup' branch

--- apps/client/src/app/auth/_components/Form/index.ts
+++ apps/client/src/app/auth/_components/Form/index.ts
@@ -1,3 +1,1 @@
-export { BusinessApiButton } from "@auth/_components/Form/BusinessApiButton";
 export { CheckBox } from "@auth/_components/Form/CheckBox";
-export { FormElement } from "@auth/_components/Form/FormElement";
