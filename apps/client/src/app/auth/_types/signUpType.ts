3ef1891e1f42e28f14a3c87b2e9848b10a2ca339
been bin <showme0241@gmail.com>
2023-11-15 00:25:34 +0900

    feat : delete 사업자 관련 schema & modify post key name <api docs>
    p.s 태호님 요청에 따라 사업자 번호 입력은 가입 단계에서 없애기 위해 관련 schema 삭제, post 해야할 key name을 api docs에 맞게 수정

--- apps/client/src/app/auth/_types/signUpType.ts
+++ apps/client/src/app/auth/_types/signUpType.ts
@@ -3,18 +3,8 @@ export type SingUpFormValues = {
   password?: string;
   passwordConfirm?: string;
   userType?: "buyer" | "seller";
-  nickName?: string;
-  tel?: string;
-  businessNum?: string;
-  isBusinessNumState?: boolean;
+  nickname?: string;
+  phoneNumber?: string;
   isAgeOverAgree?: boolean;
   isSendAdsAgree?: boolean;
 };
-
-export interface SignUpBusinessNumber {
-  tax_type: string;
-}
-
-export interface SignUpApiResponse<T> {
-  data: T[];
-}
