ca2a4f1f67ce34b9cd45f549a2e75a4bedd64145
been bin <showme0241@gmail.com>
2023-11-28 00:55:15 +0900

    feat : add RecoilRoot in Providers component & pause import msw

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -5,16 +5,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState, type PropsWithChildren } from "react";
+import { RecoilRoot } from "recoil";
 
 export const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
-  import("../mocks");
+  // import("../mocks");
 
   return (
     <QueryClientProvider client={queryClient}>
-      <CacheProvider>
-        <ChakraProvider>{children}</ChakraProvider>
-      </CacheProvider>
+      <RecoilRoot>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </RecoilRoot>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
