eb502a669dfcb9a89c6b157051f762277439026a
CodImJun <codimjun@gmail.com>
2023-12-01 15:43:37 +0900

    chore: client에 @shophub/theme 추가

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  transpilePackages: ["math-helpers"],
   images: {
     domains: ["picsum.photos"], // picsum.photos: 테스트 이미지를 위한 domain입니다.
   },
