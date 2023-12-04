97002101c0a79b161f627fc9ddd143d4f360091f
been bin <showme0241@gmail.com>
2023-12-04 22:30:06 +0900

    chore : add require sign of label <SignUpForm component>

--- apps/client/src/app/auth/signup/step2/_components/SignUpForm.tsx
+++ apps/client/src/app/auth/signup/step2/_components/SignUpForm.tsx
@@ -8,24 +8,24 @@ interface RoleProp extends FormProps {
 export const SignUpForm = ({ control, role }: RoleProp) => {
   return (
     <Flex flexDir="column" rowGap="40px">
-      <FormElement control={control} name="email" label="이메일 계정" placeholder="이메일 계정을 입력해주세요" />
+      <FormElement control={control} name="email" label="이메일 계정 *" placeholder="이메일 계정을 입력해주세요" />
       <FormElement
         control={control}
         name="password"
-        label="비밀번호"
+        label="비밀번호 *"
         type="password"
         placeholder="비밀번호를 입력해주세요"
       />
       <FormElement
         control={control}
         name="passwordConfirm"
-        label="비밀번호 확인"
+        label="비밀번호 확인 *"
         type="password"
         placeholder="비밀번호를 재입력해주세요"
       />
-      <FormElement control={control} name="nickname" label="닉네임" placeholder="닉네임을 입력해주세요" />
+      <FormElement control={control} name="nickname" label="닉네임 *" placeholder="닉네임을 입력해주세요" />
       {role === "USER_SELLER" && (
-        <FormElement control={control} name="phoneNumber" label="연락처" placeholder="연락처를 입력해주세요" />
+        <FormElement control={control} name="phoneNumber" label="연락처 *" placeholder="연락처를 입력해주세요" />
       )}
     </Flex>
   );
