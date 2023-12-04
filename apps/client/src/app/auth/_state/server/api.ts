6e2bff71698b4f23eac766289227185273b78270
been bin <showme0241@gmail.com>
2023-12-04 23:15:16 +0900

    feat : delete axios 인스턴스 생성 전 api 로직

--- apps/client/src/app/auth/_state/server/api.ts
+++ apps/client/src/app/auth/_state/server/api.ts
@@ -1,22 +1,8 @@
-import type { SignInFormValues, SignUpApiResponse, SignUpBusinessNumber } from "@auth/_types";
+import type { SignInFormValues } from "@auth/_types";
 import axios from "axios";
-
-export const signUpApi = {
-  businessConfirm: (value: string) => {
-    return axios.post<SignUpApiResponse<SignUpBusinessNumber>>(
-      `https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=jLYDm8UrTI6O0xwYjloDybofTULlg9YFEVIbA8pyRkS78TmOmeB%2B%2FlVlBWBqqoOtBAiEn%2FyukJGWMuMJrMdt0w%3D%3D`,
-      { b_no: [value] },
-    );
-  },
-
-  signUpResponse: (postData: object) => {
-    return axios.post("/api/signup", { postData });
-  },
-};
 
 export const LoginApi = {
   signInResponse: ({ email, password }: SignInFormValues) => {
     return axios.post("/api/login", { email, password });
   },
 };
-
