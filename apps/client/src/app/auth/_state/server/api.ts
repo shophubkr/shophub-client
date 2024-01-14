700d51660f3cd94e210552f27d7eed5a86fae089
been bin <showme0241@gmail.com>
2024-01-14 15:17:00 +0900

    feat : add authApi { signup : () => AxiosRequest }

--- apps/client/src/app/auth/_state/server/api.ts
+++ apps/client/src/app/auth/_state/server/api.ts
@@ -1,8 +1,18 @@
-import type { SignInFormValues } from "@auth/_types";
 import axios from "axios";
+import { createAxiosInstance } from "~/app/shared/server/axios";
+import type { SignUpFormValues } from "../../_types";
+import type { SignInFormValues } from "../../_types/signInType";
 
 export const LoginApi = {
   signInResponse: ({ email, password }: SignInFormValues) => {
     return axios.post("/api/login", { email, password });
+  },
+};
+
+export const authApi = {
+  signUp: (userInfo: SignUpFormValues) => {
+    return createAxiosInstance.post("/api/v1/auth/join", JSON.stringify(userInfo), {
+      headers: { "Content-Type": "application/json" },
+    });
   },
 };
