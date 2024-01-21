a8e8b4d127f2b8c51aab19330770465aea9ad877
CodImJun <codimjun@gmail.com>
2024-01-21 19:49:30 +0900

    chore: size props 옵셔널로 변경

--- apps/client/src/components/Icon/Icon.types.ts
+++ apps/client/src/components/Icon/Icon.types.ts
@@ -2,6 +2,6 @@ import type { ShophubThemeProps } from "@shophub/theme";
 
 export interface IconProps {
   name: string;
-  size: number;
+  size?: number;
   color?: Pick<ShophubThemeProps, "COLORS"> | string;
 }
