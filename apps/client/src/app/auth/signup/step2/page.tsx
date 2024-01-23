9ee94a0848a397df4d560ddd2e7e52259bafd22e
been bin <showme0241@gmail.com>
2024-01-23 10:43:18 +0900

    feat : update SignUpSecond component <signup/step2>

--- apps/client/src/app/auth/signup/step2/page.tsx
+++ apps/client/src/app/auth/signup/step2/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { SIGN_UP_BUYER_SCHEMA, SIGN_UP_SELLER_SCHEMA } from "@auth/_constants";
-import { signUpApi } from "@auth/_state/server/api";
-import type { SignUpFormValues } from "@auth/_types";
-import { Button, Center, Flex, Heading } from "@chakra-ui/react";
+import { useSignUpSubmitHandler } from "@auth/_state/server";
+import type { SignUpFormValues, UserType } from "@auth/_types";
+import { Button, Center, Heading } from "@chakra-ui/react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { CheckBox, FormElement } from "~/components";
+import { AgreeCheckBox, SignUpForm } from "./_components";
 
 const SignUpSecond = () => {
   const role = useSearchParams().get("userType");
@@ -25,18 +25,7 @@ const SignUpSecond = () => {
     },
   });
 
-  const onSubmitHandler = async (data: SignUpFormValues) => {
-    console.log(data);
-    const postData = { ...data, role };
-
-    try {
-      // mocking test completed
-      const res = await signUpApi.signUpResponse(postData as object);
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { onSubmitHandler } = useSignUpSubmitHandler(role as UserType);
 
   return (
     <Center w="full" flexDir="column" rowGap="80px">
@@ -44,29 +33,8 @@ const SignUpSecond = () => {
         회원 가입
       </Heading>
       <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <Flex flexDir="column" rowGap="40px">
-          <FormElement control={control} name="email" label="이메일 계정" placeholder="이메일 계정" />
-          <FormElement control={control} name="password" label="비밀번호" type="password" placeholder="비밀번호" />
-          <FormElement
-            control={control}
-            name="passwordConfirm"
-            label="비밀번호 확인"
-            type="password"
-            placeholder="비밀번호 확인"
-          />
-          <FormElement control={control} name="nickname" label="닉네임" placeholder="닉네임" />
-          {role === "USER_SELLER" && (
-            <FormElement control={control} name="phoneNumber" label="연락처" placeholder="연락처" />
-          )}
-        </Flex>
-        <Flex mt="48px" flexDir="column" rowGap="8px">
-          <CheckBox control={control} name="isAgeOverAgree">
-            [필수] 만 14세 이상이며 모두 동의합니다.
-          </CheckBox>
-          <CheckBox control={control} name="isSendAdsAgree">
-            [선택] 광고성 정보 수신에 모두 동의합니다.
-          </CheckBox>
-        </Flex>
+        <SignUpForm control={control} name="signUpForm" />
+        <AgreeCheckBox control={control} name="signUpCheckbox" />
         <Button w="full" h="48px" mt="48px" type="submit">
           가입하기
         </Button>
