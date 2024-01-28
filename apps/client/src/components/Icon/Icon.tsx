bb41d95aeafb5e613ad589272fd633a5b0535754
CodImJun <codimjun@gmail.com>
2024-01-29 00:06:51 +0900

    feat: Icon 기본 사이즈 추가

--- apps/client/src/components/Icon/Icon.tsx
+++ apps/client/src/components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import type { IconProps } from "./Icon.types";
 
 export const Icon = ({ name, size, color }: IconProps) => {
   return (
-    <span className="material-icons-outlined" style={{ fontSize: `${size}px`, color: `${color}` }}>
+    <span className="material-icons-outlined" style={{ fontSize: `${size ?? 16}px`, color: `${color}` }}>
       {name}
     </span>
   );
