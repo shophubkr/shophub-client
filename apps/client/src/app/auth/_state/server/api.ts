7380162093d8e9fc209f07949e9da959d6a37883
been bin <showme0241@gmail.com>
2024-02-03 19:02:53 +0900

    feat : add response type test
    - 공통된 result 객체를 매번 작성하지 않습니다.
    - 응답값 타입을 추론시키기 위해 axiosInstance<제네릭>으로 응답 타입을 전달합니다.

--- apps/client/src/app/auth/_state/server/api.ts
+++ apps/client/src/app/auth/_state/server/api.ts
@@ -12,7 +12,7 @@ export const signUpApi = {
 export const API_LOGIN = {
   // TODO: 테스트 작업이며, 변경될 수 있는 사항입니다.
   Request: async ({ email, password }: SignInFormValues) => {
-    const { data } = await axiosInstance().post<SignInResponse>("/auth/login", {
+    const { data } = await axiosInstance<SignInResponse>().post("/auth/login", {
       email,
       password,
     });
--- apps/client/src/app/auth/_types/signInResponse.type.ts
+++ apps/client/src/app/auth/_types/signInResponse.type.ts
@@ -1,4 +1,5 @@
 export interface SignInResponse {
   // TODO: 테스트 작업이며, 변경될 수 있는 사항입니다.
-  result: { accessToken: string; refreshToken: string };
+  accessToken: string;
+  refreshToken: string;
 }
