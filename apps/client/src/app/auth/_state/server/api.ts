662f346eb4600224b936a129175bf65c4964dae1
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-05 13:00:00 +0900

    chore: signIn api에 예시로 적용

--- apps/client/src/app/auth/_state/server/api.ts
+++ apps/client/src/app/auth/_state/server/api.ts
@@ -1,7 +1,7 @@
-import type { SignInFormValues } from "@auth/_types";
-import type { SignInResponse } from "@auth/_types/signInResponse.type";
+import type { SignInFormValues, SignInResponse } from "@auth/_types";
 import axios from "axios";
-import { axiosInstance } from "~/app/shared/server";
+import axiosInstance from "~/app/shared/server/axios/axios";
+import type { BaseResponse } from "~/app/shared/server/types";
 
 export const signUpApi = {
   signUpResponse: (postData: object) => {
@@ -12,7 +12,7 @@ export const signUpApi = {
 export const API_LOGIN = {
   // TODO: 테스트 작업이며, 변경될 수 있는 사항입니다.
   Request: async ({ email, password }: SignInFormValues) => {
-    const { data } = await axiosInstance().post<SignInResponse>("/auth/login", {
+    const { data } = await axiosInstance.post<BaseResponse<SignInResponse>>("/auth/login", {
       email,
       password,
     });
