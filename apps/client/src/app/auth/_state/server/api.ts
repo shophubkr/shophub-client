50dc7f2a114b3683a93f96129479755e84565771
been bin <showme0241@gmail.com>
2024-02-06 00:16:19 +0900

    feat : add login mutation hook for api managing

--- apps/client/src/app/auth/_state/server/api.ts
+++ apps/client/src/app/auth/_state/server/api.ts
@@ -9,8 +9,7 @@ export const signUpApi = {
 };
 
 export const API_LOGIN = {
-  // TODO: 테스트 작업이며, 변경될 수 있는 사항입니다.
-  Request: async (value: SignInFormValues) => {
+  postLogin: async (value: SignInFormValues) => {
     const { data } = await api.post<SignInResponse>("/auth/login", value);
 
     return data.result;
