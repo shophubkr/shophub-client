0ec9cbea43fe79fc3af5a9cdf291db1fbab9ec8d
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-06 16:52:08 +0900

    chore: isNotLastElement 유틸 함수 추가 후 리팩터링

--- apps/client/src/components/ListLayout/ListLayout.tsx
+++ apps/client/src/components/ListLayout/ListLayout.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import type { PropsWithChildren } from "react";
 import { Children, Fragment } from "react";
 import { HorizontalLine } from "../HorizontalLine";
+import { isNotLastElement } from "~/utils";
 
 export const ListLayout = ({ children }: PropsWithChildren) => {
   const childrenArray = Children.toArray(children);
@@ -11,7 +12,7 @@ export const ListLayout = ({ children }: PropsWithChildren) => {
       {childrenArray.map((child, index) => (
         <Fragment key={child.toString()}>
           {child}
-          {index < childrenArray.length - 1 && <HorizontalLine h="1px" />}
+          {isNotLastElement(childrenArray.length, index) && <HorizontalLine h="1px" />}
         </Fragment>
       ))}
     </Flex>
