741d52a3c2c217a2d68681ee72ff34ec0aff5e3c
been bin <showme0241@gmail.com>
2024-02-09 19:18:44 +0900

    chore : 하위 컴포넌트 props 전달과 퍼블리싱 용도로 만든 상수를 하나로 축약

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,78 +1,65 @@
 "use client";
 
 import { Container } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import { CustomOverlayMap } from "react-kakao-maps-sdk";
-import { StoreItem } from "~/components";
-import { Accordion } from "~/components/Accordion/Accordion";
-import { KakaoMap } from "~/components/Map/KakaoMap";
-import { ShopPin } from "~/components/ShopPin/ShopPin";
-import { TabBar } from "~/components/TabBar/TabBar";
-import { RoutingBar } from "./(home)/_components";
+import { Accordion, ShopPin, StoreItem, TabBar } from "~/components";
+import { useBooleanState, useMap } from "~/hooks";
+import { KakaoMap, RoutingBar } from "./(home)/_components";
 
 export default function Home() {
-  // TODO : API 조회 방식으로 변경 예정 <퍼블리싱용>
-  const shopDataArray = [
-    {
-      "id": 1,
-    },
-    {
-      "id": 2,
-    },
-    {
-      "id": 3,
-    },
-    {
-      "id": 4,
-    },
-    {
-      "id": 5,
-    },
-    {
-      "id": 6,
-    },
-  ];
+  const { value: isOpenAccordion, onChange } = useBooleanState();
+  const { isKakaoLoaded } = useMap();
+  const router = useRouter();
 
   const TEST_IMAGE_URL = "https://picsum.photos/id/237/96/96";
-
-  // TODO : API 조회로 변경합니다. <퍼블리싱용>
-  const storeInformation = {
-    thumbnailUrl: TEST_IMAGE_URL,
-    name: "BEEN STORE",
-    description: "스트릿 옷가게",
-    distance: "12m",
-    address: "서울특별시 강남구 테헤란로 11111",
-    isCouponAvailable: true,
-    minimumPrice: 14000,
-  };
 
   // TODO : API 조회로 변경합니다. <퍼블리싱용>
   const storeInformationArray = [
     {
       id: 1,
-      name: "상점 이름3",
-      address: "상점 주소",
-      introduce: "상점 소개",
-      checkCoupon: true,
+      thumbnailUrl: TEST_IMAGE_URL,
+      name: "BEEN STORE",
+      description: "스트릿 옷가게",
+      distance: "12m",
+      address: "서울특별시 강남구 테헤란로 11111",
+      isCouponAvailable: false,
+      minimumPrice: 14000,
+      latLng: { lat: 37.5934062, lng: 127.0884679 },
+    },
+    {
+      id: 2,
+      thumbnailUrl: TEST_IMAGE_URL,
+      name: "BEEN STORE",
+      description: "스트릿 옷가게",
+      distance: "12m",
+      address: "서울특별시 강남구 테헤란로 11111",
+      isCouponAvailable: false,
+      minimumPrice: 14000,
       latLng: { lat: 37.5934062, lng: 127.0884679 },
     },
   ];
 
+  const onPathRoutingHandler = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <Container w="390px" h="100vh" m="0 auto" p="8px 0 0 0">
       <RoutingBar />
-      <KakaoMap>
-        {storeInformationArray.map((store) => (
-          <CustomOverlayMap key={store.id} position={store.latLng}>
-            <ShopPin name={store.name} checkCoupon={store.checkCoupon} />
+      <KakaoMap isKakaoLoaded={isKakaoLoaded}>
+        {storeInformationArray.map((shop) => (
+          <CustomOverlayMap key={shop.id} position={shop.latLng}>
+            <ShopPin name={shop.name} checkCoupon={shop.isCouponAvailable} />
           </CustomOverlayMap>
         ))}
       </KakaoMap>
-      <Accordion>
-        {shopDataArray.map((shop) => (
-          <StoreItem key={shop.id} storeInformation={storeInformation} />
+      <Accordion value={isOpenAccordion} onChange={onChange}>
+        {storeInformationArray.map((shop) => (
+          <StoreItem key={shop.id} storeInformation={shop} />
         ))}
       </Accordion>
-      <TabBar />
+      <TabBar onPathRoutingHandler={onPathRoutingHandler} />
     </Container>
   );
 }
