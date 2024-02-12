edd1d43fbb540460ab1cbbd498dcae88951610cd
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-12 23:51:08 +0900

    chore: SummarySection 컴포넌트에 empty 값에 따라 텍스트 보여주기

--- apps/client/src/app/my/_components/SummarySection/SummarySection.tsx
+++ apps/client/src/app/my/_components/SummarySection/SummarySection.tsx
@@ -1,20 +1,31 @@
 "use client";
 
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
-import type { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
+import { useShophubTheme } from "@shophub/theme";
 import type { SummarySectionProps } from "./SummarySection.types";
 
-export const SummarySection = ({ title, href, children }: PropsWithChildren<SummarySectionProps>) => {
+export const SummarySection = ({ title, href, empty, children }: PropsWithChildren<SummarySectionProps>) => {
+  const theme = useShophubTheme();
+
   return (
-    <Box p="40px 16px">
+    <Box>
       <Flex alignItems="center" justifyContent="space-between" mb="24px">
         <Text fontWeight="700">{title}</Text>
         <Text fontSize="14px">
           <Link href={href}>더보기</Link>
         </Text>
       </Flex>
-      <section>{children}</section>
+      {!empty ? (
+        <section>{children}</section>
+      ) : (
+        <Center w="full" h="100px" boxShadow="xs" borderRadius="8px">
+          <Text textAlign="center" color={theme.COLORS.grey[700]} fontSize="14px">
+            {title}이 없습니다.
+          </Text>
+        </Center>
+      )}
     </Box>
   );
 };
