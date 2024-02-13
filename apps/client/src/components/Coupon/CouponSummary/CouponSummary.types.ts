d3717e6433dc484ee983c130948d82642efe2455
김도은 <doeunnkimm@gmail.com>
2024-02-13 23:54:45 +0900

    CouponSummary 컴포넌트 구성 방식 변경 (#68)
    # Describe your changes
    
    #### 고봉밥 PR을 예방하고자 먼저 올립니다 🙏
    
    - 복잡하게 구성되었던 CouponSummary 컴포넌트 구성 방식 변경했습니다.
    - 적용하는 과정에서 mock 데이터 쪽에서 타입 에러가 발생했는데, 이제는 사용하지 않는 코드라 우선 sign 쪽
    Index.ts 끌어서 에러는 막아두었습니다.
    - 지난 번에 변경했던 Coupon 컴포넌트 타입에 맞게 스토리 수정했습니다.
    
    ## Issue ticket number and link (optional)
    
    ## Checklist
    
    - [x] 🤔 이 프로젝트의 스타일 가이드를 따르나요?
    - [x] 🤔 머지하기 전에 스스로 코드에 문제가 없음을 확인했나요?
    - [x] 🤔 필요한 주석을 필요한 곳에 넣어주었나요?
    - [x] ⛔️ (필수) base 브랜치를 pull 받았나요?!
    
    ## Next Step Todo (optional)
    
    ## Screenshots (optional)
    
    ## Questions
    
    - 💬 질문 사항이에요!
    - 🤷‍♂️ 확인 받고 싶은 부분이에요!
    - 🔥 이건 꼭 확인해주세요!
    
    수정된 부분이 자잘하게 많아 커밋 단위로 봐주시면 감사하겠습니다,, 😓

--- apps/client/src/components/Coupon/CouponSummary/CouponSummary.types.ts
+++ apps/client/src/components/Coupon/CouponSummary/CouponSummary.types.ts
@@ -1,14 +1,5 @@
-export type CouponSummaryKeys = "total" | "used" | "unused" | "expired"; // response되는 데이터에 따라 달라질 수 있습니다.
-
-export interface CouponSummaryProps {
-  couponSummary: {
-    [key in CouponSummaryKeys]?: number;
-  };
+export interface CouponSummaryItemProps {
+  title: string;
+  count: number;
+  variant: "red" | "black" | "gray";
 }
-
-export type CouponSummaryInformationProps = {
-  [key in CouponSummaryKeys]: {
-    title: string;
-    countTextColor: string;
-  };
-};
