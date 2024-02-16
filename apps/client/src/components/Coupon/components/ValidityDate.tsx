2687549476afcf374f704ff5a443c889dc318ff8
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-16 23:48:07 +0900

    chore: 쿠폰 관련 컴포넌트 일부 수정

--- apps/client/src/components/Coupon/components/ValidityDate.tsx
+++ apps/client/src/components/Coupon/components/ValidityDate.tsx
@@ -9,11 +9,11 @@ import type { ValidityProps } from "./CouponComponents.types";
  * @param end 'YYYY-MM-DD'
  */
 export const Validity = ({ start, end }: ValidityProps) => {
-  const modifiedStart = start.split("-").join(". ");
-  const modifiedEnd = end.split("-").join(". ");
+  const modifiedStart = start.split("-").join(".");
+  const modifiedEnd = end.split("-").join(".");
   return (
     <Text fontSize="14px" color="#AAAAAA">
-      유효기간 : {modifiedStart} ~ {modifiedEnd}
+      유효기간: {modifiedStart} ~ {modifiedEnd}
     </Text>
   );
 };
