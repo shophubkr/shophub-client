d3d0263c8080c730168cd970ca8a5d5f3325346f
김도은 <doeunnkimm@gmail.com>
2024-02-20 12:57:48 +0900

    Coupon 관련 잡다한 리팩터링 작업 (#73)
    # Describe your changes
    
    - 변경된 api 명세에 따라 타입 및 퍼블리싱용 데이터 수정
    - 지난 번에 수정된 url명에 따라 이동하는 router 주소 변경
    - 컴포넌트에서 세부 스타일 수정
    
    ## Issue ticket number and link (optional)
    
    ## Checklist
    
    - [x] 🤔 이 프로젝트의 스타일 가이드를 따르나요?
    - [x] 🤔 머지하기 전에 스스로 코드에 문제가 없음을 확인했나요?
    - [x] 🤔 필요한 주석을 필요한 곳에 넣어주었나요?
    - [x] ⛔️ (필수) base 브랜치를 pull 받았나요?!
    
    ## Next Step Todo (optional)
    
    ## Screenshots (optional)
    
    ## Questions
    
    - 💬 질문 사항이에요!
    - 🤷‍♂️ 확인 받고 싶은 부분이에요!
    - 🔥 이건 꼭 확인해주세요!

--- apps/client/src/components/Coupon/components/CouponBody.tsx
+++ apps/client/src/components/Coupon/components/CouponBody.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { Flex, Text } from "@chakra-ui/react";
-import Link from "next/link";
 import type { CouponBodyProps } from "./CouponComponents.types";
 
 export const CouponBody = ({ title, isExpired }: CouponBodyProps) => {
   return (
-    <Link href={`/myapp/buyer/couponList/${title}`}>
-      <Flex w="full" h="101px" alignItems="center" justifyContent="center" bgColor="#EEEEEE" borderRadius="8px">
-        <Text fontSize="24px" fontWeight="700" color={!isExpired ? "#000" : "#AAAAAA"}>
-          {title}
-        </Text>
-      </Flex>
-    </Link>
+    <Flex w="full" h="101px" alignItems="center" justifyContent="center" bgColor="#EEEEEE" borderRadius="8px">
+      <Text fontSize="24px" fontWeight="700" color={!isExpired ? "#000" : "#AAAAAA"}>
+        {title}
+      </Text>
+    </Flex>
   );
 };
