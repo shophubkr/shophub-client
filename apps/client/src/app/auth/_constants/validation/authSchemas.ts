5e73c6bb3a4b55a9d55dfe58bd7690106362c23c
been bin <showme0241@gmail.com>
2024-02-25 12:51:11 +0900

    chore : modify yup schema <domain : auth>

--- apps/client/src/app/auth/_constants/validation/authSchemas.ts
+++ apps/client/src/app/auth/_constants/validation/authSchemas.ts
@@ -1,28 +1,24 @@
-import {
-  email,
-  isAgeOverAgree,
-  isSendAdsAgree,
-  nickname,
-  password,
-  passwordConfirm,
-  phoneNumber,
-} from "~/app/shared/validation";
+import * as yup from "yup";
+import { email, isAgeOverAgree, isSendAdsAgree, password, passwordConfirm, phoneNumber } from "~/app/shared/validation";
 
-export const SIGN_UP_BUYER_SCHEMA = {
+export const SIGN_UP_COMMON_SCHEMA = {
   email,
   password,
   passwordConfirm,
-  nickname,
   isAgeOverAgree,
   isSendAdsAgree,
 };
 
-export const SIGN_UP_SELLER_SCHEMA = {
-  ...SIGN_UP_BUYER_SCHEMA,
-  phoneNumber,
-};
+export const SIGN_UP_BUYER_SCHEMA = yup.object({
+  ...SIGN_UP_COMMON_SCHEMA,
+});
 
-export const LOGIN_SCHEMA = {
+export const SIGN_UP_SELLER_SCHEMA = yup.object({
+  ...SIGN_UP_COMMON_SCHEMA,
+  phoneNumber,
+});
+
+export const LOGIN_SCHEMA = yup.object({
   email,
   password,
-};
+});
